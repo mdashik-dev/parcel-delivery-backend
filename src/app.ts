f7bd2f9b14feb4a6c9f427a28419ca0c2c3ce515
Md Ashik <mdashik8495@gmail.com>
2025-09-01 15:16:25 +0600

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ const app = express()
 app.use(cookieParser());
 app.use(express.json())
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: *",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"]
