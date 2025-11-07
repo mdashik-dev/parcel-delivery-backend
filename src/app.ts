8aeb106a69055c401112cc046fad11f4cf703d8a
Md Ashik <mdashik8495@gmail.com>
2025-11-07 08:30:47 +0600

    Update CORS origin to localhost:3000

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ const app = express()
 app.use(cookieParser());
 app.use(express.json())
 app.use(cors({
-  origin: *",
+  origin: "http://localhost:3000",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"]
