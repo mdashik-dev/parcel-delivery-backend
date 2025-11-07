9479a034675a6bb3e942ab0941b8bca7a98c5120
Md Ashik <mdashik8495@gmail.com>
2025-11-07 09:24:18 +0600

    Add maxAge to auth cookies for expiration

--- src/app/utils/setAuthCookie.ts
+++ src/app/utils/setAuthCookie.ts
@@ -9,6 +9,7 @@ export const setAuthCookie = (res: Response, tokenInfo: AuthTokens) => {
     if (tokenInfo.accessToken) {
         res.cookie("accessToken", tokenInfo.accessToken, {
             httpOnly: true,
+            maxAge: 15 * 60 * 1000,
             secure: true
         })
     }
@@ -16,6 +17,7 @@ export const setAuthCookie = (res: Response, tokenInfo: AuthTokens) => {
     if (tokenInfo.refreshToken) {
         res.cookie("refreshToken", tokenInfo.refreshToken, {
             httpOnly: true,
+            maxAge: 15 * 24 * 60 * 60 * 1000,
             secure: true,
         })
     }
